c924a7fc60233adf1446b7c8de81f4ccd3d248a7
Håkan Arnoldson <hakan@arnoldson.net>
2023-12-04 15:42:02 +0100

    Day 4: Scratchcards

--- 2023/Day4/Day4.sql
+++ 2023/Day4/Day4.sql
@@ -38,6 +38,8 @@ into #Card
 from wo
 cross apply string_split(replace(NrSet,N'  ',N' '),N' ', 1)
 
+--Part 1 Answer:
+
 select sum(Score) from (
 select distinct CardID
 ,Score = power(2,count(Nr) over (partition by CardID) - 1)
@@ -50,3 +52,85 @@ from (
 						   and c1.Nr = c2.Nr
 		) t
 ) t
+
+drop table if exists #Cards
+create table #Cards (
+		 ID int not null identity(1,1) primary key
+		,CardID int not null
+		,Processed bit not null default(0)
+	   )
+	   
+drop table if exists #Numbers
+create table #Numbers (
+		CardID int not null
+	   ,SetType nvarchar(20)  not null
+	   ,Nr int not null
+	   primary key(CardID,SetType,Nr))
+
+set identity_insert #Cards on
+
+insert into #Cards (ID, CardID, Processed)
+select distinct ID = CardID,CardID, Processed = 0 from #Card
+
+set identity_insert #Cards off
+
+insert into #Numbers (
+		CardID
+	   ,SetType
+	   ,Nr)
+select CardID 
+	,SetType
+	,Nr 
+from #Card
+
+
+drop table if exists #WinMatrix
+create table #WinMatrix (
+	 ID int not null primary key
+	,CardID int not null
+	,Wins int not null
+	,WinStartCardID int not null
+	,WinEndCardID int not null
+)
+
+--loop lazy
+while exists(select 1 from #Cards where Processed = 0) 
+begin
+	truncate table #WinMatrix
+
+	insert into #WinMatrix (
+		 ID
+		,CardID
+		,Wins
+		,WinStartCardID
+		,WinEndCardID)
+	select ID				= c.ID
+		 , CardID			= c.CardID
+		 , Wins				= count(n1.Nr) 
+		 , WinStartCardID	= c.CardID + 1
+		 , WinEndCardID		= c.CardID + count(n1.Nr) 	 
+	from #Cards c
+	inner join #Numbers n1 on c.CardID = n1.CardID
+	inner join #Numbers n2 on n1.CardID = n2.CardID
+							   and n1.SetType <> n2.SetType
+							   and n1.Nr = n2.Nr
+	where c.Processed = 0
+	  and n1.SetType = 'Winners'
+	group by c.ID,c.CardID
+
+	update #Cards set Processed = 1
+
+
+	insert #Cards (CardID, Processed)
+	select CardID = c.CardID
+		  ,Processed = 0
+	from #WinMatrix w
+	inner join #Cards c on w.WinStartCardID <= c.ID and w.WinEndCardID >= c.ID
+	
+
+
+end
+
+--Answer Part 2
+
+select count(*) from #Cards
