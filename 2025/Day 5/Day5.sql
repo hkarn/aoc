9298d35ed4593adba19f2f76b7a96654a144d570
Håkan Arnoldson <hakan@arnoldson.net>
2025-12-21 16:03:47 +0100

    Day 5: Cafeteria Part 2
    Modified the #Ranges table structure to include RangeID as a primary key. Added logic to extend ranges based on overlaps and created a distinct ranges table.

--- 2025/Day 5/Day5.sql
+++ 2025/Day 5/Day5.sql
@@ -23,10 +23,10 @@ openrowset(
 /* --- Day 5: Cafeteria --- */
 
 drop table if exists #Ranges
-create table #Ranges (RangeID int not null identity(1,1) 
+create table #Ranges (RangeID int not null identity(1,1)  
 					, FromIngredientID bigint not null
 					, ToIngredientID bigint not null
-					primary key (FromIngredientID, ToIngredientID))
+					primary key (FromIngredientID, ToIngredientID,RangeID))
 drop table if exists #Ingredients
 create table #Ingredients (IngredientID bigint not null primary key)
 
@@ -49,3 +49,46 @@ from #Ingredients i
 where exists(select 1/0 from #Ranges r
 						where r.FromIngredientID <= i.IngredientID
 						  and r.ToIngredientID >= i.IngredientID)
+
+
+/* --- Part 2 --- */
+declare @WordDone bit = 1
+while @WordDone = 1
+begin
+set @WordDone = 0
+/* extend ranges ending inside another range */
+update r1 set  r1.ToIngredientID = r2.ToIngredientID
+/* select * --*/
+from #Ranges r1
+inner join #Ranges r2
+						on r1.ToIngredientID >= r2.FromIngredientID
+						  and r1.ToIngredientID <= r2.ToIngredientID
+						  and r1.RangeID <> r2.RangeID
+where r1.ToIngredientID <> r2.ToIngredientID
+
+if rowcount_big() > 0 set @WordDone = 1
+
+/* extend start of ranges starting inside another range */
+update r1 set  r1.FromIngredientID = r2.FromIngredientID
+/* select * --*/
+from #Ranges r1
+inner join #Ranges r2
+						on r1.FromIngredientID <= r2.ToIngredientID
+						  and r1.FromIngredientID >= r2.FromIngredientID
+						  and r1.RangeID <> r2.RangeID
+where r1.FromIngredientID <> r2.FromIngredientID
+
+if rowcount_big() > 0 set @WordDone = 1
+end
+
+/* distinct ranges */
+drop table if exists #RangesDist
+select distinct
+	FromIngredientID
+	,ToIngredientID
+into #RangesDist
+from #Ranges
+
+
+select sum(ToIngredientID-FromIngredientID+1) from 
+#RangesDist r
