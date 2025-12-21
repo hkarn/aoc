ee3d9daee42298237a59f852a805706426262995
Håkan Arnoldson <hakan@arnoldson.net>
2025-12-21 11:45:11 +0100

    Create SQL script for data processing and analysis
    This SQL script processes input data from a CSV file, creating a temporary table to stage the data, and then cleans and analyzes it to count specific conditions related to neighboring values.

---
+++ 2025/Day 4/Day4.sql
@@ -0,0 +1,60 @@
+set xact_abort, nocount on;
+
+drop table if exists #input_stage
+create table #input_stage
+(
+	 LineID int not null identity(1,1) primary key
+	,txt nvarchar(max) null
+)
+insert into #input_stage(txt)
+select txt from 
+openrowset(
+	 bulk  '/input_data/input.txt'
+	,fieldterminator = ''
+	,rowterminator = '\r\n'
+	,format = 'csv'
+	,firstrow = 1
+) with (txt nvarchar(max)
+) input
+
+drop table if exists #inputClean;
+
+with numbers as (
+  select n = 1
+		,maxLn = (select max(len(txt)) from #input_stage)
+  union all
+  select n = n + 1
+	    ,maxLn
+  from numbers 
+  where n < maxLn
+)
+select 
+   yID = s.LineID
+  ,xID = n
+  ,Content = substring(s.txt, n, 1)
+into #inputClean
+from #input_stage s
+cross join numbers 
+where n <= len(s.txt)
+option (maxrecursion 0);
+
+with neighbours as (
+select Orig_yID = o.yID
+	  ,Orig_xID = o.xID
+	  ,HasRoll = case when o.Content = '@' then 1 else 0 end
+	  ,NeighbourHasRoll = case when p.Content = '@' then 1 else 0 end
+from #inputClean o
+inner join #inputClean p on (p.yID = o.yID - 1 or p.yID = o.yID + 1 or p.yID = o.yID)
+						and (p.xID = o.xID - 1 or p.xID = o.xID + 1 or p.xID = o.xID)
+						and not (p.yID = o.yID and p.xID = o.xID)
+)
+,nSum as (
+select yID = Orig_yID
+      ,xID = Orig_xID
+	  ,rollSum = sum(NeighbourHasRoll)
+from neighbours
+where HasRoll = 1
+group by Orig_yID,Orig_xID
+)
+select count(*) from nSum
+where rollSum < 4
