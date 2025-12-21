e40ec5a11716e0e6d9811048448c2597b9772a39
Håkan Arnoldson <hakan@arnoldson.net>
2025-12-21 11:16:33 +0100

    Update Day3.sql

--- 2025/Day 3/Day3.sql
+++ 2025/Day 3/Day3.sql
@@ -76,3 +76,57 @@ group by s.BankID)
 select sum(cast(concat(BatA,BatB) as int))
 from BatA a
 inner join BatB b on a.BankID = b.BankID 
+
+--Part 2
+drop table if exists #State
+create table #State( BankID int primary key, LastIx int)
+drop table if exists #Result
+create table #Result (BankID int, Pos int, Joltage int)
+
+insert into #State (BankID, LastIx)
+select distinct BankID, 0 from #inputClean 
+
+/* GREEDY LOOP. All banks are 100 long. We need to preserver 12 digits. Look to <= 89 */
+declare @Step int = 1
+       ,@TotalSteps int = 12
+       ,@TotalLength int = 100;
+
+while @Step <= @TotalSteps
+begin
+with ranked as (
+select
+     i.BankID
+    ,i.BatteryID
+    ,i.Joltage
+    ,rnk = row_number() over (partition by i.BankID order by i.Joltage desc, i.BatteryID asc)
+from #inputClean i
+join #State s on s.BankID = i.BankID
+where i.BatteryID > s.LastIx
+  and i.BatteryID <= @TotalLength - (@TotalSteps - @Step)
+)
+,best as (
+select BankID, Joltage, BatteryID
+from ranked where rnk = 1
+)
+merge #state as tgt
+using best as src
+on tgt.BankID = src.BankID
+when matched then
+update set LastIx = src.BatteryID
+output src.BankID,@Step,src.Joltage into #Result(BankID, Pos, Joltage);
+
+set @Step = @Step + 1;
+end
+
+with a as (
+select
+    BankID,
+    BankValue = cast(string_agg(cast(Joltage as varchar(1)), '') within group (order by Pos) as bigint)
+from #Result
+group by BankID
+)
+select Part2_Answer = sum(BankValue)
+from a
+
+
+
